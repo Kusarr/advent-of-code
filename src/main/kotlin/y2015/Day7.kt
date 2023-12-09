91aa9bab4c1ce94f4d58e6d9cdd95daec2c81589
Sven Wollmershäuser <sven.wollmi@web.de>
2023-12-09 17:01:12 +0100

    setup all files for all available years

---
+++ src/main/kotlin/y2015/Day7.kt
@@ -0,0 +1,21 @@
+package y2015
+
+import FileUtil
+
+fun main() {
+    val lines = FileUtil().readLines("2015/day7-input.txt")
+    part1(lines)
+    part2(lines)
+}
+
+private fun part1(lines: List<String>) {
+    val result = null
+
+    println("Result Part1: $result")
+}
+
+private fun part2(lines: List<String>) {
+    val result = null
+
+    println("Result Part2: $result")
+}
---
+++ src/main/kotlin/y2016/Day18.kt
@@ -0,0 +1,21 @@
+package y2016
+
+import FileUtil
+
+fun main() {
+    val lines = FileUtil().readLines("2016/day18-input.txt")
+    part1(lines)
+    part2(lines)
+}
+
+private fun part1(lines: List<String>) {
+    val result = null
+
+    println("Result Part1: $result")
+}
+
+private fun part2(lines: List<String>) {
+    val result = null
+
+    println("Result Part2: $result")
+}
