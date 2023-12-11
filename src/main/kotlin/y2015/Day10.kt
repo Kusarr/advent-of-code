272c1983bb4952f5328bafc40afb4dbae89b8add
Sven Wollmershäuser <sven.wollmi@web.de>
2023-12-11 01:19:31 +0100

    day 10 2015

--- src/main/kotlin/y2015/Day10.kt
+++ src/main/kotlin/y2015/Day10.kt
@@ -1,21 +1,30 @@
 package y2015
 
-import FileUtil
-
 fun main() {
-    val lines = FileUtil().readLines("2015/day10-input.txt")
-    part1(lines)
-    part2(lines)
+    val input = "1321131112"
+    part1(input)
+    part2(input)
 }
 
-private fun part1(lines: List<String>) {
-    val result = null
+private val regex = Regex("(\\d)\\1*")
+
+private fun part1(input: String) {
+    val result = execute(input, 40)
 
     println("Result Part1: $result")
 }
 
-private fun part2(lines: List<String>) {
-    val result = null
+private fun part2(input: String) {
+    val result = execute(input, 50)
 
     println("Result Part2: $result")
+}
+
+private fun execute(input: String, times: Int): Int {
+    var puzzle = input
+    for (i in 0..<times) {
+        puzzle = regex.findAll(puzzle).toList()
+            .joinToString("") { it.groups[0]!!.value.length.toString() + it.groups[1]!!.value }
+    }
+    return puzzle.length
 }
