b5ae00197f2564223373c97f66198ddd24eb2a55
Sven Wollmershäuser <sven.wollmi@web.de>
2023-12-13 17:42:10 +0100

    day 11 2015

--- src/main/kotlin/y2015/Day11.kt
+++ src/main/kotlin/y2015/Day11.kt
@@ -1,21 +1,62 @@
 package y2015
 
-import FileUtil
+private val forbiddenChars = setOf('i', 'o', 'l')
 
 fun main() {
-    val lines = FileUtil().readLines("2015/day11-input.txt")
-    part1(lines)
-    part2(lines)
+    part1("cqjxjnds")
+    part2("cqjxxyzz")
 }
 
-private fun part1(lines: List<String>) {
-    val result = null
+private fun part1(input: String) {
+    val password = getPassowrd(input)
 
-    println("Result Part1: $result")
+    println("Result Part1: $password")
 }
 
-private fun part2(lines: List<String>) {
-    val result = null
+private fun part2(input: String) {
+    val password = getPassowrd(input)
 
-    println("Result Part2: $result")
+    println("Result Part2: $password")
+}
+
+private fun getPassowrd(input: String): String {
+    val password = input.toCharArray()
+
+    do {
+        increase(password, password.size - 1)
+    } while (!passwordValid(password))
+
+    return password.joinToString("")
+}
+
+private fun increase(password: CharArray, index: Int) {
+    if (password[index] == 'z') {
+        password[index] = 'a'
+        increase(password, index - 1)
+    } else {
+        password[index] = password[index].inc()
+    }
+
+    if (forbiddenChars.contains(password[index])) {
+        password[index] = password[index].inc()
+    }
+}
+
+fun passwordValid(password: CharArray): Boolean {
+    var straightIncreases = 0
+    var pairs = 0
+    var firstPairChar = '.'
+    var prevChar = '.'
+
+    for (char in password) {
+        if (forbiddenChars.contains(char)) return false
+        if (prevChar == char && firstPairChar != char) {
+            pairs++
+            firstPairChar = char
+        }
+        if (prevChar.inc() == char) straightIncreases++ else if (straightIncreases < 2) straightIncreases = 0
+        prevChar = char
+    }
+
+    return pairs > 1 && straightIncreases > 1
 }
