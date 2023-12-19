7c84316cefca650dfa5afe46fdb38d5fb63836c9
Sven Wollmershäuser <sven.wollmi@web.de>
2023-12-19 01:26:32 +0100

    day 13 2015

--- src/main/kotlin/y2015/Day13.kt
+++ src/main/kotlin/y2015/Day13.kt
@@ -9,50 +9,65 @@ fun main() {
 }
 
 private fun part1(lines: List<String>) {
-    val relations = lines.map { line ->
-        val terms = line.split(" ")
-        val score = if (terms[2] == "lose") terms[3].toInt() * -1 else terms[3].toInt()
-        val personNextTo = terms.last().replace(".", "")
-        Relation(terms[0], personNextTo, score)
-    }.sortedByDescending { it.score }.toList()
+    val happinessMap = createSeatMap(lines)
 
-    val attendees = mutableListOf(relations[0].person1, relations[0].person2)
-    relations.drop(1).forEach { rel ->
-        if (attendees.containsAll(listOf(rel.person1, rel.person2))) return@forEach
-        if (attendees.first() == rel.person1) {
-            attendees.addFirst(rel.person2)
-            return@forEach
-        }
-        if (attendees.first() == rel.person2) {
-            attendees.addFirst(rel.person1)
-            return@forEach
-        }
-        if (attendees.last() == rel.person1) {
-            attendees.add(rel.person2)
-            return@forEach
-        }
-        if (attendees.last() == rel.person2) {
-            attendees.add(rel.person1)
-            return@forEach
-        }
-    }
+    val result = findBestSeating(happinessMap)
 
-    var result = 0
-    for (i in attendees.indices) {
-        val nextIndex = if (i == attendees.size - 1) 0 else i + 1
-        val prevIndex = if (i == 0) attendees.size - 1 else i - 1
-        result += relations.filter { it.person1 == attendees[i] && (it.person2 == attendees[nextIndex] || it.person2 == attendees[prevIndex]) }
-            .sumOf { it.score }
-    }
-
-    // 674 too low
     println("Result Part1: $result")
 }
 
 private fun part2(lines: List<String>) {
-    val result = null
+    val happinessMap = createSeatMap(lines)
+
+    val guests = happinessMap.keys.toList()
+    guests.forEach { guest ->
+        happinessMap.getOrPut("me") { mutableMapOf() }[guest] = 0
+        happinessMap.getOrPut(guest) { mutableMapOf() }["me"] = 0
+    }
+
+    val result = findBestSeating(happinessMap)
 
     println("Result Part2: $result")
 }
 
-private data class Relation(val person1: String, val person2: String, val score: Int)
+private fun findBestSeating(happinessMap: MutableMap<String, MutableMap<String, Int>>): Int {
+    val guests = happinessMap.keys.toList()
+    var maxHappiness = Int.MIN_VALUE
+
+    val permutations = guests.permute()
+
+    for (permutation in permutations) {
+        var totalHappiness = 0
+
+        for (i in permutation.indices) {
+            val currentGuest = permutation[i]
+            val nextGuest = permutation[(i + 1) % permutation.size]
+
+            totalHappiness += happinessMap[currentGuest]?.get(nextGuest) ?: 0
+            totalHappiness += happinessMap[nextGuest]?.get(currentGuest) ?: 0
+        }
+
+        if (totalHappiness > maxHappiness) {
+            maxHappiness = totalHappiness
+        }
+    }
+    return maxHappiness
+}
+
+private fun createSeatMap(lines: List<String>): MutableMap<String, MutableMap<String, Int>> {
+    val happinessMap = mutableMapOf<String, MutableMap<String, Int>>()
+    lines.forEach { line ->
+        val terms = line.split(" ")
+        val score = if (terms[2] == "lose") terms[3].toInt() * -1 else terms[3].toInt()
+        val personNextTo = terms.last().replace(".", "")
+        happinessMap.getOrPut(terms[0]) { mutableMapOf() }[personNextTo] = score
+    }
+    return happinessMap
+}
+
+// Extension function to generate all permutations of a list
+fun <T> List<T>.permute(): List<List<T>> =
+    if (size <= 1) listOf(this)
+    else flatMap { element ->
+        (this - element).permute().map { permutation -> listOf(element) + permutation }
+    }
