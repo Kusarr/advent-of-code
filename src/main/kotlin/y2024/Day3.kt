9d286df23c74f3692654a4a06c2718797a45c8ad
Sven Wollmershäuser <sven.wollmi@web.de>
2024-12-05 23:33:13 +0100

    init 2024

---
+++ src/main/kotlin/y2024/Day3.kt
@@ -0,0 +1,21 @@
+package y2024
+
+import FileUtil
+
+fun main() {
+    val lines = FileUtil().readLines("2024/day3-input.txt")
+    part1(lines)
+    part2(lines)
+}
+
+private fun part1(lines: List<String>) {
+    val result = null
+
+    println("Result Part1: $result")
+}
+
+private fun part2(lines: List<String>) {
+    val result = null
+
+    println("Result Part2: $result")
+}
