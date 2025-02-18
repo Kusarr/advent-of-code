c877b68207977c833cfe1e3e1cb68c016c233d0d
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-02-18 18:36:31 +0100

    2015 day 20 part 1

--- src/main/kotlin/y2015/Day20.kt
+++ src/main/kotlin/y2015/Day20.kt
@@ -1,20 +1,40 @@
 package y2015
 
-import FileUtil
+import kotlin.math.sqrt
+
+private const val INPUT = 34_000_000
+private const val STEP = 10
+
+private var startingHouse = sqrt(INPUT * 2.0 / STEP).toInt()
 
 fun main() {
-    val lines = FileUtil().readLines("2015/day20-input.txt")
-    part1(lines)
-    part2(lines)
+    part1()
+    part2()
 }
 
-private fun part1(lines: List<String>) {
-    val result = null
+private fun part1() {
+    var currentHouse = startingHouse - 1
+    var result = STEP
 
-    println("Result Part1: $result")
+    while (result < INPUT) {
+        result = STEP + currentHouse * STEP
+        currentHouse++
+
+        for (i in 2..sqrt(currentHouse.toDouble()).toInt()) {
+            if (currentHouse % i == 0) {
+                result += i * STEP
+                val div = currentHouse / i
+                if (div != i) result += div * STEP
+            }
+        }
+
+        println("House:$currentHouse -> Result: $result")
+    }
+
+    println("Result Part1: $currentHouse")
 }
 
-private fun part2(lines: List<String>) {
+private fun part2() {
     val result = null
 
     println("Result Part2: $result")
