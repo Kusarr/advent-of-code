cd03ddb9cab1d87f6d6b060aefcb8736e9e896c6
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-07-16 15:51:54 +0200

    2016 day 3 and 4

--- src/main/kotlin/y2016/Day3.kt
+++ src/main/kotlin/y2016/Day3.kt
@@ -1,6 +1,7 @@
 package y2016
 
 import FileUtil
+import numberRegex
 
 fun main() {
     val lines = FileUtil().readLines("2016/day3-input.txt")
@@ -9,13 +10,30 @@ fun main() {
 }
 
 private fun part1(lines: List<String>) {
-    val result = null
+    val result = lines.asSequence()
+        .map { line -> numberRegex.findAll(line).map { it.value.toInt() } }
+        .map { isTriangle(it.toList()) }
+        .count { it }
 
     println("Result Part1: $result")
 }
 
 private fun part2(lines: List<String>) {
-    val result = null
+    val result = lines.asSequence()
+        .map { numberRegex.findAll(it).map { m -> m.value.toInt() }.toList() }
+        .chunked(3)  // group rows into groups of 3
+        .flatMap { triple ->
+            (0..2).map { i ->
+                listOf(triple[0][i], triple[1][i], triple[2][i])
+            }
+        }
+        .count { isTriangle(it) }
 
     println("Result Part2: $result")
+}
+
+private fun isTriangle(numbers: List<Int>): Boolean {
+    return numbers[0] + numbers[1] > numbers[2]
+            && numbers[0] + numbers[2] > numbers[1]
+            && numbers[1] + numbers[2] > numbers[0]
 }
--- src/main/kotlin/y2016/Day4.kt
+++ src/main/kotlin/y2016/Day4.kt
@@ -3,19 +3,61 @@ package y2016
 import FileUtil
 
 fun main() {
-    val lines = FileUtil().readLines("2016/day4-input.txt")
-    part1(lines)
-    part2(lines)
+    val rooms = FileUtil().readLines("2016/day4-input.txt")
+        .asSequence()
+        .map { it ->
+            val name = it.substring(0, it.lastIndexOf('-'))
+            val id = it.substring(it.lastIndexOf('-') + 1, it.indexOf('['))
+            val checksum = it.substring(it.indexOf('[') + 1, it.indexOf(']'))
+            Room(name, id.toInt(), checksum)
+        }.toList()
+
+    part1(rooms)
+    part2(rooms)
 }
 
-private fun part1(lines: List<String>) {
-    val result = null
+private fun part1(rooms: List<Room>) {
+    val result = rooms
+        .filter { it.isReal() }
+        .sumOf { it.id }
 
     println("Result Part1: $result")
 }
 
-private fun part2(lines: List<String>) {
-    val result = null
+private fun part2(rooms: List<Room>) {
+    val result = rooms.find { room -> room.decryptedName() == "northpole object storage" }?.id
 
     println("Result Part2: $result")
+}
+
+private data class Room(val name: String, val id: Int, val checksum: String) {
+
+    fun mostUsedChars(): List<Pair<Char, Int>> {
+        return name.replace("-", "")
+            .groupingBy { it }
+            .eachCount()
+            .toList()
+            .sortedWith(compareByDescending<Pair<Char, Int>> { it.second }.thenBy { it.first })
+    }
+
+    fun isReal(): Boolean {
+        val topChars = mostUsedChars().take(5).map { it.first }.toSet()
+        return checksum.all { topChars.contains(it) }
+    }
+
+    fun decryptedName(): String {
+        // a-z = 97-122
+        val shift = getCharShift()
+        return name.split("-").joinToString(" ") {
+            it.map { char -> shiftChar(char, shift) }.joinToString("")
+        }
+    }
+
+    fun shiftChar(c: Char, shift: Int): Char {
+        return 'a' + (c - 'a' + shift) % 26
+    }
+
+    fun getCharShift(): Int {
+        return id % 26
+    }
 }
