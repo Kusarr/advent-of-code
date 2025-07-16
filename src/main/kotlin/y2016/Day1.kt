3c9b52f62f417c39a2d2936c78291db8eeed8e1b
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-07-16 12:16:17 +0200

    2016 day 1 and 2

--- src/main/kotlin/y2016/Day1.kt
+++ src/main/kotlin/y2016/Day1.kt
@@ -1,21 +1,64 @@
 package y2016
 
 import FileUtil
+import kotlin.math.abs
+
 
 fun main() {
-    val lines = FileUtil().readLines("2016/day1-input.txt")
-    part1(lines)
-    part2(lines)
+    val line = FileUtil().readLines("2016/day1-input.txt")[0].split(",").map { it.trim() }
+    part1(line)
+    part2(line)
 }
 
 private fun part1(lines: List<String>) {
-    val result = null
+    val directions = mutableListOf(0, 0, 0, 0) // N,E,S,W
+    var currentDir = 0
+
+    lines.forEach {
+        currentDir = if (it[0] == 'L') (currentDir - 1).sizedIndex(4) else (currentDir + 1).sizedIndex(4)
+        directions[currentDir] = directions[currentDir] + it.substring(1).toInt()
+    }
+
+    val result = abs(directions[0] - directions[2]) + abs(directions[1] - directions[3])
 
     println("Result Part1: $result")
 }
 
 private fun part2(lines: List<String>) {
-    val result = null
+    val size = 500
+    val startPoint = size / 2
+    val coordinates: Array<Array<Char>> = Array(size) { Array(size) { '□' } }
+    var x = startPoint
+    var y = startPoint
+    coordinates[y][x] = 'X'
 
-    println("Result Part2: $result")
+    var currentDir = 0
+
+    lines.forEach {
+        currentDir = if (it[0] == 'L') (currentDir - 1).sizedIndex(4) else (currentDir + 1).sizedIndex(4)
+
+        for (i in 1..it.substring(1).toInt()) {
+            when (currentDir) {
+                0 -> y--
+                1 -> x++
+                2 -> y++
+                3 -> x--
+            }
+            if (coordinates[y][x] == 'X') {
+                val result = abs(x - startPoint) + abs(y - startPoint)
+                println("Result Part2: $result")
+                return
+            }
+
+            coordinates[y][x] = 'X'
+        }
+
+    }
+    throw IllegalStateException("No location reached two times. Should not happen!")
+}
+
+fun Int.sizedIndex(size: Int): Int {
+    if (this < 0) return size - 1
+    if (this > 3) return 0
+    return this
 }
--- src/main/kotlin/y2016/Day2.kt
+++ src/main/kotlin/y2016/Day2.kt
@@ -1,6 +1,9 @@
 package y2016
 
 import FileUtil
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val lines = FileUtil().readLines("2016/day2-input.txt")
@@ -9,13 +12,48 @@ fun main() {
 }
 
 private fun part1(lines: List<String>) {
-    val result = null
+    val code = mutableListOf<Pair<Int, Int>>()
+    var x = 1
+    var y = 1
+    lines.forEach { line ->
+        line.toCharArray().forEach { char ->
+            when (char) {
+                'U' -> y = min(2, y + 1)
+                'D' -> y = max(0, y - 1)
+                'R' -> x = min(2, x + 1)
+                'L' -> x = max(0, x - 1)
+            }
+        }
+        code.add(x to y)
+    }
 
-    println("Result Part1: $result")
+    println("Result Part1: $code")
 }
 
 private fun part2(lines: List<String>) {
-    val result = null
+    val code = mutableListOf<Pair<Int, Int>>()
+    var x = -2
+    var y = 0
+    lines.forEach { line ->
+        line.toCharArray().forEach { char ->
+            when (char) {
+                'U' -> y = min(getMax(x), y + 1)
+                'D' -> y = max(getMax(x) * -1, y - 1)
+                'R' -> x = min(getMax(y), x + 1)
+                'L' -> x = max(getMax(y) * -1, x - 1)
+            }
+        }
+        code.add(x to y)
+    }
 
-    println("Result Part2: $result")
+    println("Result Part2: $code")
+}
+
+private fun getMax(otherAxis: Int): Int {
+    return when (abs(otherAxis)) {
+        0 -> 2
+        1 -> 1
+        2 -> 0
+        else -> throw IllegalArgumentException("Houston we have a problem")
+    }
 }
