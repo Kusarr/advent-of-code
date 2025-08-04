529c0a2ef59da2cd1733ca94805615dad541e234
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-08-04 18:32:52 +0200

    2017 day 1

--- src/main/kotlin/y2017/Day1.kt
+++ src/main/kotlin/y2017/Day1.kt
@@ -3,19 +3,22 @@ package y2017
 import FileUtil
 
 fun main() {
-    val lines = FileUtil().readLines("2017/day1-input.txt")
-    part1(lines)
-    part2(lines)
+    val line = FileUtil().readLines("2017/day1-input.txt")[0]
+    part1(line)
+    part2(line)
 }
 
-private fun part1(lines: List<String>) {
-    val result = null
+private fun part1(line: String) {
+    val result = (line + line[0]).zipWithNext()
+        .sumOf { (a, b) -> if (a == b) a.digitToInt() else 0 }
 
     println("Result Part1: $result")
 }
 
-private fun part2(lines: List<String>) {
-    val result = null
+private fun part2(line: String) {
+    val halfs = line.chunked(line.length / 2)
+    val result = halfs[0].zip(halfs[1])
+        .sumOf { (a, b) -> if (a == b) a.digitToInt() else 0 } * 2
 
     println("Result Part2: $result")
 }
