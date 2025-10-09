b51666070ad3be1ebe1b3ffb0f8c7ba72c933e75
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-10-09 12:38:33 +0200

    2017 day 5

--- src/main/kotlin/y2017/Day5.kt
+++ src/main/kotlin/y2017/Day5.kt
@@ -4,18 +4,35 @@ import FileUtil
 
 fun main() {
     val lines = FileUtil().readLines("2017/day5-input.txt")
+        .map { it.toInt() }.toList()
     part1(lines)
     part2(lines)
 }
 
-private fun part1(lines: List<String>) {
-    val result = null
-
-    println("Result Part1: $result")
+private fun part1(lines: List<Int>) {
+    val steps = countSteps(lines) { jumps, index, _ -> jumps[index]++ }
+    println("Result Part1: $steps")
 }
 
-private fun part2(lines: List<String>) {
-    val result = null
+private fun part2(lines: List<Int>) {
+    val steps = countSteps(lines) { jumps, index, currNumber ->
+        if (currNumber > 2) jumps[index]-- else jumps[index]++
+    }
 
-    println("Result Part2: $result")
+    println("Result Part2: $steps")
+}
+
+private fun countSteps(lines: List<Int>, updateRule: (MutableList<Int>, Int, Int) -> Unit): Int {
+    val jumps = lines.toMutableList()
+    var index = 0
+    var steps = 0
+
+    while (index >= 0 && index < jumps.size) {
+        val currNumber = jumps[index]
+        updateRule(jumps, index, currNumber)
+        index += currNumber
+        steps++
+    }
+
+    return steps
 }
