148a79c0ed47e49ff7a99026de067c1ecef7d8e6
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-12-04 18:39:10 +0100

    year 2025 day 2

--- src/main/kotlin/y2025/Day2.kt
+++ src/main/kotlin/y2025/Day2.kt
@@ -2,20 +2,31 @@ package y2025
 
 import FileUtil
 
+private val line = FileUtil().readLines("2025/day2-input.txt")[0]
+
+private val part1Regex = Regex("^(\\d+)\\1\$")
+private val part2Regex = Regex("^(\\d+)(\\1)+\$")
+
 fun main() {
-    val lines = FileUtil().readLines("2025/day2-input.txt")
-    part1(lines)
-    part2(lines)
+    part1()
+    part2()
 }
 
-private fun part1(lines: List<String>) {
-    val result = null
-
+private fun part1() {
+    val result = sumUpInvalidIds(part1Regex)
     println("Result Part1: $result")
 }
 
-private fun part2(lines: List<String>) {
-    val result = null
-
+private fun part2() {
+    val result = sumUpInvalidIds(part2Regex)
     println("Result Part2: $result")
+}
+
+private fun sumUpInvalidIds(regex: Regex): Long {
+    return line.split(",")
+        .map { it.split("-") }
+        .map { LongRange(it[0].toLong(), it[1].toLong()) }
+        .sumOf { range ->
+            range.filter { regex.matches(it.toString()) }.sum()
+        }
 }
