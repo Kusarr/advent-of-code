7d62cf6f12c49175e0bb323605f40032e4b97979
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-12-04 15:43:10 +0100

    create setup for 2025

---
+++ src/main/kotlin/y2025/Day5.kt
@@ -0,0 +1,21 @@
+package y2025
+
+import FileUtil
+
+fun main() {
+    val lines = FileUtil().readLines("2019/day5-input.txt")
+    part1(lines)
+    part2(lines)
+}
+
+private fun part1(lines: List<String>) {
+    val result = null
+
+    println("Result Part1: $result")
+}
+
+private fun part2(lines: List<String>) {
+    val result = null
+
+    println("Result Part2: $result")
+}
