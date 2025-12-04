cc926571d5fbee88d9a700d7d5d1c5cd048a1d94
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-12-04 18:07:49 +0100

    year 2025 day 1

--- src/main/kotlin/y2025/Day1.kt
+++ src/main/kotlin/y2025/Day1.kt
@@ -9,13 +9,38 @@ fun main() {
 }
 
 private fun part1(lines: List<String>) {
-    val result = null
+    var dial = 50
+    var result = 0
+
+    for (line in lines) {
+        val direction = line[0]
+        var distance = line.drop(1).toInt()
+
+        if (direction == 'L') distance = -distance
+
+        dial = Math.floorMod(dial + distance, 100)
+
+        if (dial == 0) result++
+    }
 
     println("Result Part1: $result")
 }
 
 private fun part2(lines: List<String>) {
-    val result = null
+    var dial = 50
+    var result = 0
+
+    for (line in lines) {
+        val dir = line[0]
+        val dist = line.substring(1).toInt()
+
+        val step = if (dir == 'L') -1 else 1
+
+        repeat(dist) {
+            dial = (dial + step + 100) % 100
+            if (dial == 0) result++
+        }
+    }
 
     println("Result Part2: $result")
 }
