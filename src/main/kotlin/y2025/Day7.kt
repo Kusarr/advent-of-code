a6bfdbba1530b1f1c394a3d54c2b0e4885508fee
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-12-09 09:19:55 +0100

    year 2025 day 7

--- src/main/kotlin/y2025/Day7.kt
+++ src/main/kotlin/y2025/Day7.kt
@@ -9,13 +9,53 @@ fun main() {
 }
 
 private fun part1(lines: List<String>) {
-    val result = null
+    var rays = mutableSetOf<Int>()
+    rays.add(lines[0].indexOf('S'))
+
+    var result = 0
+
+    lines.forEach { line ->
+        val newRays = mutableSetOf<Int>()
+        line.forEachIndexed { i, char ->
+            if (char == '^' && rays.contains(i)) {
+                newRays.add(i - 1)
+                newRays.add(i + 1)
+                result++
+            } else if (rays.contains(i)) {
+                newRays.add(i)
+            }
+        }
+        rays = newRays
+    }
 
     println("Result Part1: $result")
 }
 
 private fun part2(lines: List<String>) {
-    val result = null
+    var rays = MutableList(lines[0].length) { 0L }
+    rays[(lines[0].indexOf('S'))] = 1
 
+
+    lines.forEach { line ->
+        val newRays = MutableList(lines[0].length) { 0L }
+        line.forEachIndexed { i, char ->
+            if (char == '^' && rays[i] > 0) {
+                newRays[i - 1] += rays[i]
+                newRays[i + 1] += rays[i]
+            } else if (rays[i] > 0) {
+                newRays[i] += rays[i]
+            }
+        }
+
+        // debug
+        println(rays)
+        println(line.toCharArray().toList())
+        println(newRays)
+        println()
+
+        rays = newRays
+    }
+
+    val result = rays.sum()
     println("Result Part2: $result")
 }
