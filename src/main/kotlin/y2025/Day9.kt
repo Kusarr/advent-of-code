a9dd02d8061c303c863e6dc438d3862d5be04011
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-12-10 08:16:45 +0100

    year 2025 day 9 part 1

--- src/main/kotlin/y2025/Day9.kt
+++ src/main/kotlin/y2025/Day9.kt
@@ -1,6 +1,7 @@
 package y2025
 
 import FileUtil
+import kotlin.math.abs
 
 fun main() {
     val lines = FileUtil().readLines("2025/day9-input.txt")
@@ -9,7 +10,16 @@ fun main() {
 }
 
 private fun part1(lines: List<String>) {
-    val result = null
+    val redTiles = lines.map { RedTile(it) }
+
+    var result = 0L
+    for (i in redTiles.indices) {
+        for (j in i + 1 until redTiles.size) {
+            val x = abs(redTiles[i].x - redTiles[j].x) + 1
+            val y = abs(redTiles[i].y - redTiles[j].y) + 1
+            result = maxOf(result, x * y)
+        }
+    }
 
     println("Result Part1: $result")
 }
@@ -18,4 +28,16 @@ private fun part2(lines: List<String>) {
     val result = null
 
     println("Result Part2: $result")
+}
+
+private data class RedTile(val line: String) {
+
+    val x: Long
+    val y: Long
+
+    init {
+        val (x, y) = line.split(",")
+        this.x = x.toLong()
+        this.y = y.toLong()
+    }
 }
