cb538267593da0ccb745a0b70e3c471e7b8f79eb
Sven Wollmershäuser <sven.wollmershaeuser@diva-e.com>
2025-12-10 07:51:26 +0100

    year 2025 day 8 part 2

--- src/main/kotlin/y2025/Day8.kt
+++ src/main/kotlin/y2025/Day8.kt
@@ -10,6 +10,40 @@ fun main() {
 }
 
 private fun part1(lines: List<String>) {
+    val sortedDistances = getSortedDistances(lines)
+
+    val curcuites = mutableListOf<MutableSet<Box>>()
+    sortedDistances.subList(0, 1000).forEach { connectBoxes(curcuites, it) }
+
+    curcuites.sortByDescending { it.size }
+    val result = curcuites.subList(0, 3).fold(1) { acc, cur -> acc * cur.size }
+
+    println("Result Part1: $result")
+}
+
+private fun part2(lines: List<String>) {
+    val sortedDistances = getSortedDistances(lines)
+
+    val curcuites = mutableListOf<MutableSet<Box>>()
+    for (dist in sortedDistances) {
+        connectBoxes(curcuites, dist)
+
+        if (curcuites.size == 1 && curcuites.first().size == lines.size) {
+            val result = dist.second.x.toLong() * dist.third.x.toLong()
+            println("Result Part2: $result")
+            break
+        }
+    }
+}
+
+private fun distance(a: Box, b: Box): Double {
+    val dx = a.x - b.x
+    val dy = a.y - b.y
+    val dz = a.z - b.z
+    return sqrt(dx * dx + dy * dy + dz * dz)
+}
+
+private fun getSortedDistances(lines: List<String>): List<Triple<Double, Box, Box>> {
     val boxes = lines.map { Box(it) }
 
     val distances = mutableListOf<Triple<Double, Box, Box>>()
@@ -20,45 +54,25 @@ private fun part1(lines: List<String>) {
         }
     }
 
-    val sortedDistances = distances.sortedWith(compareBy { it.first })
+    return distances.sortedWith(compareBy { it.first })
+}
 
-    val curcuites = mutableListOf<MutableSet<Box>>()
-    sortedDistances.subList(0, 1000).forEach { dist ->
-        val curcuitA = curcuites.find { cur -> dist.second in cur }
-        val curcuitB = curcuites.find { cur -> dist.third in cur }
+private fun connectBoxes(curcuites: MutableList<MutableSet<Box>>, dist: Triple<Double, Box, Box>) {
+    val curcuitA = curcuites.find { cur -> dist.second in cur }
+    val curcuitB = curcuites.find { cur -> dist.third in cur }
 
-        if (curcuitA != null && curcuitB != null && curcuitA != curcuitB) {
-            curcuitA.addAll(curcuitB)
-            curcuites.remove(curcuitB)
-        } else if (curcuitA == null && curcuitB == null) {
-            val newCurcuit = mutableSetOf<Box>()
-            newCurcuit.add(dist.second)
-            newCurcuit.add(dist.third)
-            curcuites.add(newCurcuit)
-        } else {
-            curcuitA?.add(dist.third)
-            curcuitB?.add(dist.second)
-        }
-
+    if (curcuitA != null && curcuitB != null && curcuitA != curcuitB) {
+        curcuitA.addAll(curcuitB)
+        curcuites.remove(curcuitB)
+    } else if (curcuitA == null && curcuitB == null) {
+        val newCurcuit = mutableSetOf<Box>()
+        newCurcuit.add(dist.second)
+        newCurcuit.add(dist.third)
+        curcuites.add(newCurcuit)
+    } else {
+        curcuitA?.add(dist.third)
+        curcuitB?.add(dist.second)
     }
-
-    curcuites.sortByDescending { it.size }
-    val result = curcuites.subList(0, 3).fold(1) { acc, cur -> acc * cur.size }
-
-    println("Result Part1: $result")
-}
-
-private fun part2(lines: List<String>) {
-    val result = null
-
-    println("Result Part2: $result")
-}
-
-private fun distance(a: Box, b: Box): Double {
-    val dx = a.x - b.x
-    val dy = a.y - b.y
-    val dz = a.z - b.z
-    return sqrt(dx * dx + dy * dy + dz * dz)
 }
 
 private data class Box(val line: String) {
